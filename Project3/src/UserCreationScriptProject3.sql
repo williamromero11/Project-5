f089d58a3389944e2d58da91c21084f20bcff8d7
William  Romero <wi424039@ucf.edu>
2026-03-08 10:56:16 -0400

    Delete Project3 directory

--- Project3/src/UserCreationScriptProject3.sql
+++
@@ -1,6 +0,0 @@
-# this script creates the client-level users for CNT 4714 Project 3 for Spring 2026
-
-create user "client1" identified with sha256_password by "client1";
-create user "client2" identified with sha256_password by "client2";
-create user "project3app" identified with sha256_password by "project3app";
-create user "theaccountant" identified with sha256_password by "theaccountant";
